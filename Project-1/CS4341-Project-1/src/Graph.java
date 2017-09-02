41e8c4a8b18a56c4f66315cf2d5d030bd1590a25
Akshit (Axe) Soota <akshitsoota@users.noreply.github.com>
2017-09-02 12:01:55 -0400

    [Node Code] Commit with Graph, GraphNode and tests

---
+++ Project-1/CS4341-Project-1/src/Graph.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Graph {
+    private static final boolean IS_GRAPH_UNDIRECTED_BY_DEFAULT = true;
+
+    private final Map<String, GraphNode> nodeMapping;
+
+    public Graph() {
+        this.nodeMapping = new HashMap<>();
+    }
+
+    public final void addConnection(final String from, final String to, final double connectionWeight) {
+        addConnection(from, to, connectionWeight, IS_GRAPH_UNDIRECTED_BY_DEFAULT);
+    }
+
+    public final void addConnection(final String from, final String to, final double connectionWeight, final boolean twoWayConnection) {
+        final GraphNode fromNode;
+        if (!nodeMapping.containsKey(from)) {
+            fromNode = new GraphNode(from);
+            nodeMapping.put(from, fromNode);
+        } else {
+            fromNode = nodeMapping.get(from);
+        }
+
+        final GraphNode toNode;
+        if (!nodeMapping.containsKey(to)) {
+            toNode = new GraphNode(to);
+            nodeMapping.put(to, toNode);
+        } else {
+            toNode = nodeMapping.get(to);
+        }
+
+        fromNode.addOutgoingNode(toNode, connectionWeight);
+
+        if (twoWayConnection) {
+            toNode.addOutgoingNode(fromNode, connectionWeight);
+        }
+    }
+
+    public final void setHeuristicValue(final String nodeKey, final double heuristicValue) {
+        final GraphNode node;
+        if (nodeMapping.containsKey(nodeKey) && (node = nodeMapping.get(nodeKey)) != null) {
+            node.setHeuristicValue(heuristicValue);
+        }
+    }
+
+    public final List<GraphNode> getAllGraphNodes() {
+        final List<GraphNode> nodes = new ArrayList<>();
+        nodes.addAll(nodeMapping.values());
+
+        return nodes;
+    }
+
+    /**
+     * Determines if two nodes in the graph are <b>directly</b> connected
+     */
+    public final boolean isConnected(final String from, final String to) {
+        return isConnected(from, to, IS_GRAPH_UNDIRECTED_BY_DEFAULT);
+    }
+
+    /**
+     * Determines if two nodes in the graph are <b>directly</b> connected
+     */
+    public final boolean isConnected(final String from, final String to, final boolean twoWayConnection) {
+        final GraphNode fromNode = nodeMapping.getOrDefault(from, null);
+        final GraphNode toNode = nodeMapping.getOrDefault(to, null);
+
+        if (fromNode == null && toNode == null) {
+            return false;
+        }
+
+        final boolean fromConnectedWithToHuh = fromNode != null && fromNode.isConnectedTo(toNode);
+        if (!twoWayConnection) {
+            return fromConnectedWithToHuh;
+        }
+
+        final boolean toConnectedWithFromHuh = toNode != null && toNode.isConnectedTo(fromNode);
+        return fromConnectedWithToHuh || toConnectedWithFromHuh;
+    }
+}
---
+++ Project-1/CS4341-Project-1/src/GraphNode.java
@@ -0,0 +1,109 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class GraphNode {
+    private static final double DEFAULT_HEURISTIC_VALUE = 0.0;
+
+    private final String key;
+    private double heuristicValue;
+    private final Map<GraphNode, Double> outgoingNodes;
+
+    public GraphNode(final String key) {
+        this(key, DEFAULT_HEURISTIC_VALUE, null);
+    }
+
+    public GraphNode(final String key, final double heuristicValue) {
+        this(key, heuristicValue, null);
+    }
+
+    public GraphNode(final String key, final double heuristicValue, final Map<GraphNode, Double> outgoingNodes) {
+        this.key = key;
+        this.heuristicValue = heuristicValue;
+
+        this.outgoingNodes = new HashMap<>();
+        if (outgoingNodes != null && outgoingNodes.size() != 0) {
+            this.outgoingNodes.putAll(outgoingNodes);
+        }
+    }
+
+    public final String getKey() {
+        return key;
+    }
+
+    public void setHeuristicValue(final double heuristicValue) {
+        this.heuristicValue = heuristicValue;
+    }
+
+    public final double getHeuristicValue() {
+        return heuristicValue;
+    }
+
+    public final void addOutgoingNode(final GraphNode to, final double connectionWeight) {
+        outgoingNodes.put(to, connectionWeight);
+    }
+
+    public final Map<GraphNode, Double> getAllOutgoingNodes() {
+        final Map<GraphNode, Double> toReturn = new HashMap<>();
+        toReturn.putAll(outgoingNodes);
+
+        return toReturn;
+    }
+
+    public final boolean isConnectedTo(final GraphNode to) {
+        return to != null && (this == to || outgoingNodes.containsKey(to));
+    }
+
+    @Override
+    public int hashCode() {
+        // Ideally heuristicValue and outgoingNodes (circular dependency) should be included but....
+        return 31 + 37 * key.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof GraphNode)) {
+            return false;
+        }
+
+        final GraphNode other = GraphNode.class.cast(obj);
+        if (!this.key.equals(other.key)) {
+            return false;
+        }
+
+        if (this.heuristicValue != other.heuristicValue) {
+            return false;
+        }
+
+        if (!this.outgoingNodes.equals(other.outgoingNodes)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+
+        builder.append(key).append(" (h = ").append(heuristicValue).append(") connects to ");
+        for (final Iterator<Map.Entry<GraphNode, Double>> iterator = outgoingNodes.entrySet().iterator(); iterator.hasNext(); ) {
+            final Map.Entry<GraphNode, Double> entry = iterator.next();
+            builder.append(entry.getKey().getKey()).append("->").append(entry.getValue());
+
+            if (iterator.hasNext()) {
+                builder.append("; ");
+            }
+        }
+
+        return builder.toString();
+    }
+}
